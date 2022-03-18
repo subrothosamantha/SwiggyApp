6cadc51c77b091db92fc107e92029b1b9e393f61
subrotho samantha <subrotho.samantha@dunnsolutions.com>
2022-03-18 13:04:52 +0530

    few changes

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  slideOptions = {
+    slidesPerView:1.1
+  }
+
   constructor() { }
 
   ngOnInit() {
