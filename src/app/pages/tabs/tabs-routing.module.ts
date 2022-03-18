ae417c8381c9b6889f83438514e9c066559c7940
subrotho samantha <subrotho.samantha@dunnsolutions.com>
2022-03-18 11:38:45 +0530

    chanding colour properties

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -6,8 +6,34 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  }
+    component: TabsPage,
+    children:[
+     
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+      },
+      {
+        path: 'search',
+        loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+      },
+      {
+        path: 'account',
+        loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+      },
+      {
+        path:'',
+        redirectTo: 'home',
+        pathMatch:'full'
+      },
+    ]
+  },
+  
+
 ];
 
 @NgModule({
