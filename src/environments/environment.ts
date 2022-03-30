e0406e4cc388ecc653fb62de2449727af0b1cf8d
subrotho samantha <subrotho.samantha@dunnsolutions.com>
2022-03-30 19:22:05 +0530

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+ firebaseConfig : {
+    apiKey: "AIzaSyB3_FR6T0S4dPchMi3LxUUkfZCShgT-Is8",
+    authDomain: "swiggycloneapp-b5b3f.firebaseapp.com",
+    projectId: "swiggycloneapp-b5b3f",
+    storageBucket: "swiggycloneapp-b5b3f.appspot.com",
+    messagingSenderId: "504837356745",
+    appId: "1:504837356745:web:d054b4cb291261f41c6118",
+    measurementId: "G-X5RW2RLFB7"
+  }
 };
 
 /*
